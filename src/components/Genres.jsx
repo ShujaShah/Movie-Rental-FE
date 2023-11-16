ffe2fd3485208e80ac463d2264466b80ba7c6c47
shujashah <shuja.ashraf11@gmail.com>
2023-11-16 15:53:49 +0530

    added error handling

--- src/components/Genres.jsx
+++ src/components/Genres.jsx
@@ -1,17 +1,25 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import apiClient from '../services/api-client';
+import { Text } from '@chakra-ui/react';
+
 const Genres = () => {
   const [genres, setGenres] = useState([]);
   const [error, setError] = useState('');
 
   useEffect(() => {
-    apiClient('/genres')
+    apiClient
+      .get('/genres')
       .then((res) => setGenres(res.data))
-      .catch((error) => setError(error.response.data));
+      .catch((error) => {
+        if (error.response) {
+          setError(error.response.data.message || 'An error occurred'); // request other than 200
+        }
+      });
   }, []);
 
   return (
     <>
+      {error && <Text>Error: {error}</Text>}
       <ul>
         {genres.map((genre) => (
           <li key={genre.id}>{genre.name}</li>
