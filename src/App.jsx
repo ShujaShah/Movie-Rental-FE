5f671f55f01f63c59a1a359563cb4692029b93c1
shujashah <shuja.ashraf11@gmail.com>
2023-11-16 11:27:17 +0530

    built an responsive layout using chakra

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
+import { Button, ButtonGroup, Grid, GridItem, Show } from '@chakra-ui/react';
 import './App.css';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-const defaultTheme = createTheme();
 function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <>
-        <HomePage />
-        {/* <LoginPage /> */}
-      </>
-    </ThemeProvider>
+    <>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`, // greater than 1024
+        }}
+      >
+        <GridItem area="nav" bg="coral">
+          Navbar
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="aside" bg="green">
+            Sidebar
+          </GridItem>
+        </Show>
+        <GridItem area="main" bg="blue">
+          Main area
+        </GridItem>
+      </Grid>
+      {/* <HomePage /> */}
+      {/* <LoginPage /> */}
+    </>
   );
 }
 
--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -5,8 +5,17 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import axios from 'axios';
+import apiClient from '../services/api-client';
 
 export default function MediaCard() {
+  React.useEffect(() => {
+    getData();
+  }, []);
+  const getData = apiClient
+    .get('/api/movies')
+    .then((res) => console.log(res.data))
+    .catch((error) => console.error('Error: ', error.res.data));
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardMedia
