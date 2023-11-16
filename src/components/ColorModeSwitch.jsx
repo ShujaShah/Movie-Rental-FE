f04c9e91c707ee83456d56a1eb854fb56d6ed857
shujashah <shuja.ashraf11@gmail.com>
2023-11-16 11:59:32 +0530

    implemented chakra icon for dark theme

--- src/components/ColorModeSwitch.jsx
+++ src/components/ColorModeSwitch.jsx
@@ -1,17 +1,17 @@
 import { HStack, Switch, Text, useColorMode } from '@chakra-ui/react';
+import { MoonIcon } from '@chakra-ui/icons';
 
 const ColorModeSwitch = () => {
   const { toggleColorMode, colorMode } = useColorMode();
   return (
     <>
       <HStack>
+        <MoonIcon />
         <Switch
           colorScheme="green"
           isChecked={colorMode === 'dark'}
           onChange={toggleColorMode}
-        >
-          <Text>{colorMode}</Text>
-        </Switch>
+        ></Switch>
       </HStack>
     </>
   );
