5b9c3903bd5523b1c2434aabc06a7e6c4692e58d
shujashah <shuja.ashraf11@gmail.com>
2023-11-17 20:16:52 +0530

    added spinner for genres

--- src/components/Genres.jsx
+++ src/components/Genres.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Text } from '@chakra-ui/react';
+import { Spinner, Text } from '@chakra-ui/react';
 import useGenres from '../hooks/useGenres';
 
 const Genres = () => {
-  const { genres, error } = useGenres();
+  const { genres, error, spinner } = useGenres();
   return (
     <>
       {error && <Text>Error: {error}</Text>}
+      {spinner && <Spinner />}
       <ul>
         {genres.map((genre) => (
           <li key={genre.id}>{genre.name}</li>
--- src/hooks/useGenres.js
+++ src/hooks/useGenres.js
@@ -1,21 +1,28 @@
 import { useState, useEffect } from 'react';
 import apiClient from '../services/api-client';
+import { Spinner } from '@chakra-ui/react';
 
 const useGenres = () => {
   const [genres, setGenres] = useState([]);
   const [error, setError] = useState('');
+  const [spinner, setSpinner] = useState(false);
 
   useEffect(() => {
+    setSpinner(true);
     apiClient
       .get('/genres')
-      .then((res) => setGenres(res.data))
+      .then((res) => {
+        setGenres(res.data);
+        setSpinner(false);
+      })
       .catch((error) => {
         if (error.response) {
           setError(error.response.data.message || 'An error occurred'); // response other than 200
+          setSpinner(false);
         }
       });
   }, []);
-  return { genres, error };
+  return { genres, error, spinner };
 };
 
 export default useGenres;
