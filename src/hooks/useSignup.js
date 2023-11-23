6490a6ba12b96b716a6c1616c0d690aa8f68f23d
shujashah <shuja.ashraf11@gmail.com>
2023-11-23 23:02:09 +0530

    added auto login for signup

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -14,6 +14,7 @@ const useSignup = () => {
       .post('/users', { email, name, password })
       .then((res) => {
         console.log('Response is : ', res.data);
+        localStorage.setItem('x-auth-token', res.data.token);
         setIsLoading(false);
         toast({
           title: 'Success',
