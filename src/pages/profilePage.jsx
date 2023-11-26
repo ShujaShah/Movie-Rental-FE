0c53daf4047144d5d4f4a6575d2657358f8b519b
shujashah <shuja.ashraf11@gmail.com>
2023-11-26 19:32:59 +0530

    added the navbar to profile page

--- src/pages/profilePage.jsx
+++ src/pages/profilePage.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import Profile from '../components/Profile';
+import NavBar from '../components/NavBar';
+import useUser from '../hooks/useUser';
 
 const ProfilePage = () => {
+  const { onSearch, user, HandleLogout, HandleProfile } = useUser();
   return (
     <>
+      <NavBar
+        onSearch={onSearch}
+        user={user}
+        HandleLogout={HandleLogout}
+        HandleProfile={HandleProfile}
+      />
       <Profile />
     </>
   );
