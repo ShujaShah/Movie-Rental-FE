c6e9e518e18a6a606e2c2303fb8b62174ed450e0
shujashah <shuja.ashraf11@gmail.com>
2023-11-26 21:09:28 +0530

    implemented logic for login and signup pg

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,12 @@
 import Login from '../components/Login';
+import useUser from '../hooks/useUser';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 function LoginPage() {
+  const navigate = useNavigate();
+  const { user } = useUser();
+
+  if (user) return navigate('/');
   return (
     <>
       <Login />
--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import Signup from '../components/Signup';
+import useUser from '../hooks/useUser';
+import { useNavigate } from 'react-router-dom';
 
 const SignupPage = () => {
+  const navigate = useNavigate();
+  const { user } = useUser();
+
+  if (user) return navigate('/');
   return (
     <>
       <Signup />
