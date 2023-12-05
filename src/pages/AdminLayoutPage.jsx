4cda0897d439cf2c28fd05376868fa662e7aedb3
shujashah <shuja.ashraf11@gmail.com>
2023-12-05 12:33:06 +0530

    implemented authorization for admin

--- src/pages/AdminLayoutPage.jsx
+++ src/pages/AdminLayoutPage.jsx
@@ -1,14 +1,22 @@
 import { Outlet } from 'react-router-dom';
 import Sidebar from '../components/admin-components/sidebar';
+import useUser from '../hooks/useUser';
+import { Text } from '@chakra-ui/react';
 
 const AdminLayoutPage = () => {
+  const { user } = useUser();
+
+  if (user?.role !== 'admin')
+    return <Text>You donot have permission to view this page</Text>;
   return (
-    <div style={{ display: 'flex' }}>
-      <Sidebar />
-      <div style={{ marginLeft: '200px', padding: '20px', flexGrow: 1 }}>
-        <Outlet />
+    <>
+      <div style={{ display: 'flex' }}>
+        <Sidebar />
+        <div style={{ marginLeft: '200px', padding: '20px', flexGrow: 1 }}>
+          <Outlet />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
