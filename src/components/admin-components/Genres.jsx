76f398835f9b41c2046c8295480ab4729e9f6f53
shujashah <shuja.ashraf11@gmail.com>
2023-12-08 00:02:43 +0530

    implemented the custom component for genre

--- src/components/admin-components/Genres.jsx
+++ src/components/admin-components/Genres.jsx
@@ -23,30 +23,15 @@ import {
   Spinner,
 } from '@chakra-ui/react';
 
-// import CreateGenre from './CreateGenre';
+import CreateGenre from './CreateGenre';
 
 const Genres = () => {
-  const { genre, isloading, error, addGenre, handleGenreSubmit } = useGenre();
+  const { genre, isloading, addGenre, handleGenreSubmit } = useGenre();
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   useEffect(() => {
     onClose(); //Close the modal after clicking on register
   }, [addGenre]);
-
-  //===================================embedding the Genre Component code here for sometime====================================
-  const [name, setName] = useState('');
-
-  const initialRef = useRef();
-  const finalRef = useRef();
-
-  function handleName(e) {
-    setName(e.target.value);
-  }
-
-  const handleFormSubmit = async (e) => {
-    e.preventDefault();
-    await handleGenreSubmit(name);
-  };
 
   if (!genre) return <p>No genres</p>;
 
@@ -55,50 +40,12 @@ const Genres = () => {
   return (
     <>
       <Button onClick={onOpen}>Add Genre</Button>
-      {/* <CreateGenre
+      <CreateGenre
         isOpen={isOpen}
         onClose={onClose}
         handleSubmitAdmin={handleGenreSubmit}
         isloading={isloading}
-      /> */}
-
-      <Modal
-        initialFocusRef={initialRef}
-        finalFocusRef={finalRef}
-        isOpen={isOpen}
-        onClose={onClose}
-      >
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Enter Genres</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody pb={6}>
-            <form onSubmit={handleFormSubmit}>
-              <FormControl mt={4}>
-                <FormLabel>Name</FormLabel>
-                <Input
-                  type="text"
-                  name="name"
-                  value={name}
-                  onChange={handleName}
-                />
-              </FormControl>
-              <FormControl>
-                <Button
-                  mt={4}
-                  variant="outline"
-                  border="1px solid #8d2dab"
-                  type="submit"
-                >
-                  {isloading ? <Spinner /> : 'Submit'}
-                </Button>
-              </FormControl>
-            </form>
-          </ModalBody>
-        </ModalContent>
-      </Modal>
-      {isloading && <Spinner />}
-      {error && <p>Something went wrong</p>}
+      />
 
       <TableContainer pt={10}>
         <Table variant="simple">
