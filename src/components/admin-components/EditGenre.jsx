a7521358f03ecf9f7f1609865a1ac72ce584641a
shujashah <shuja.ashraf11@gmail.com>
2023-12-13 23:14:11 +0530

    extracted the edit genre logic to separate component

--- src/components/admin-components/EditGenre.jsx
+++ src/components/admin-components/EditGenre.jsx
@@ -1,6 +1,5 @@
-import React, { useState, useRef } from 'react';
+import React from 'react';
 import {
-  Button,
   Modal,
   ModalOverlay,
   ModalContent,
@@ -10,66 +9,52 @@ import {
   FormControl,
   FormLabel,
   Input,
+  Button,
   Spinner,
 } from '@chakra-ui/react';
 
-const EditGenre = ({ isOpen, onClose, handleEdit, isloading, genreItem }) => {
-  const [name, setName] = useState('');
-
-  const initialRef = useRef();
-  const finalRef = useRef();
-
-  function handleName(e) {
-    setName(e.target.value);
-  }
-
-  const handleFormSubmit = async (e) => {
-    e.preventDefault();
-    await handleEdit(genreItem, name);
-    onClose(); // Close the modal after submitting
-  };
-
+const EditGenre = ({
+  isOpen,
+  onClose,
+  onSubmit,
+  updateGenre,
+  handleUpdateGenre,
+  isloading,
+}) => {
   return (
-    <>
-      <Modal
-        initialFocusRef={initialRef}
-        finalFocusRef={finalRef}
-        isOpen={isOpen}
-        onClose={onClose}
-      >
-        <ModalOverlay
-          bg="blackAlpha.300"
-          backdropFilter="blur(10px) hue-rotate(90deg)"
-        />
-        <ModalContent>
-          <ModalHeader>Edit Genre</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody pb={6}>
-            <form onSubmit={handleFormSubmit}>
-              <FormControl mt={4}>
-                <FormLabel>Name</FormLabel>
-                <Input
-                  type="text"
-                  name="name"
-                  value={name}
-                  onChange={handleName}
-                />
-              </FormControl>
-              <FormControl>
-                <Button
-                  mt={5}
-                  variant="outline"
-                  border="1px solid #8d2dab"
-                  type="submit"
-                >
-                  {isloading ? <Spinner /> : 'Submit'}
-                </Button>
-              </FormControl>
-            </form>
-          </ModalBody>
-        </ModalContent>
-      </Modal>
-    </>
+    <Modal isOpen={isOpen} onClose={onClose} isCentered>
+      <ModalOverlay
+        bg="blackAlpha.300"
+        backdropFilter="blur(10px) hue-rotate(90deg)"
+      />
+      <ModalContent>
+        <ModalHeader>Edit Genre</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody pb={6}>
+          <form onSubmit={onSubmit}>
+            <FormControl mt={4}>
+              <FormLabel>Name</FormLabel>
+              <Input
+                type="text"
+                name="name"
+                value={updateGenre}
+                onChange={handleUpdateGenre}
+              />
+            </FormControl>
+            <FormControl>
+              <Button
+                mt={5}
+                variant="outline"
+                border="1px solid #8d2dab"
+                type="submit"
+              >
+                {isloading ? <Spinner /> : 'Submit'}
+              </Button>
+            </FormControl>
+          </form>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
   );
 };
 
--- src/components/admin-components/Genres.jsx
+++ src/components/admin-components/Genres.jsx
@@ -1,135 +1,3 @@
-// import React from 'react';
-// import useGenre from '../../hooks/admin-hooks/useGenre';
-// import { useEffect, useState, useRef } from 'react';
-// import {
-//   Table,
-//   Thead,
-//   Tbody,
-//   Tr,
-//   Th,
-//   Td,
-//   TableContainer,
-//   Button,
-//   useDisclosure,
-//   AlertDialog,
-// } from '@chakra-ui/react';
-
-// import CreateGenre from './CreateGenre';
-// import EditGenre from './EditGenre';
-// import { DeleteIcon, EditIcon } from '@chakra-ui/icons';
-// import AlertDelete from './AlertDelete';
-
-// const Genres = () => {
-//   const {
-//     genre,
-//     isloading,
-//     addGenre,
-//     handleGenreSubmit,
-//     handleEditGenre,
-//     handleDeleteGenre,
-//     del,
-//     editGenre,
-//   } = useGenre();
-
-//   // creating separate disclosures as we have to use more than one modal in this component..
-//   const addGenreModal = useDisclosure();
-//   const deleteGenreModal = useDisclosure();
-//   const editGenreModal = useDisclosure();
-
-//   // used by delete genre for cancel button
-//   const cancelRef = React.useRef();
-
-//   const handleEdit = async (genreItem) => {
-//     await handleEditGenre(genreItem._id, 'hell');
-//   };
-
-//   const handleDelete = async (genreItem) => {
-//     await handleDeleteGenre(genreItem);
-//   };
-
-//   useEffect(() => {
-//     addGenreModal.onClose(); //Close the modal after clicking on register
-//     deleteGenreModal.onClose(); // close the modal after clicking on delete
-//   }, [addGenre, del, editGenre]);
-
-//   if (!genre) return <p>No genres</p>;
-
-//   if (genre.length === 0) return <p>No Genres found</p>;
-
-//   return (
-//     <>
-//       <Button onClick={addGenreModal.onOpen}>Add Genre</Button>
-//       <CreateGenre
-//         isOpen={addGenreModal.isOpen}
-//         onClose={addGenreModal.onClose}
-//         handleSubmitAdmin={handleGenreSubmit}
-//         isloading={isloading}
-//       />
-
-//       <TableContainer pt={10}>
-//         <Table variant="simple">
-//           <Thead>
-//             <Tr>
-//               <Th>Genres</Th>
-//               <Th>ID</Th>
-//             </Tr>
-//           </Thead>
-//           <Tbody>
-//             {genre.map((genreItem) => (
-//               <Tr key={genreItem?._id}>
-//                 <Td>{genreItem?.name}</Td>
-//                 <Td>{genreItem?._id}</Td>
-//                 <Td>
-//                   <Button
-//                     onClick={editGenreModal.onOpen}
-//                     leftIcon={<EditIcon />}
-//                     colorScheme="blue"
-//                     variant="solid"
-//                   >
-//                     Edit
-//                   </Button>
-//                   <EditGenre
-//                     isOpen={editGenreModal.isOpen}
-//                     onClose={editGenreModal.onClose}
-//                     handleEdit={() => handleEdit(genreItem)}
-//                     isloading={isloading}
-//                     genreItem={genreItem}
-//                   />
-//                 </Td>
-//                 <Td>
-//                   <Button
-//                     colorScheme="red"
-//                     onClick={deleteGenreModal.onOpen}
-//                     leftIcon={<DeleteIcon />}
-//                     variant="solid"
-//                   >
-//                     Delete
-//                   </Button>
-//                   <AlertDialog
-//                     isOpen={deleteGenreModal.isOpen}
-//                     leastDestructiveRef={cancelRef}
-//                     onClose={deleteGenreModal.onClose}
-//                   >
-//                     {/* create a custom component for alert dialogue for delete genre */}
-//                     <AlertDelete
-//                       isOpen={deleteGenreModal.isOpen}
-//                       onClose={deleteGenreModal.onClose}
-//                       handleDelete={() => handleDelete(genreItem._id)}
-//                       cancelRef={cancelRef}
-//                     />
-//                   </AlertDialog>
-//                 </Td>
-//               </Tr>
-//             ))}
-//           </Tbody>
-//         </Table>
-//       </TableContainer>
-//     </>
-//   );
-// };
-
-// export default Genres;
-
 import React from 'react';
 import useGenre from '../../hooks/admin-hooks/useGenre';
 import { useEffect, useState, useRef } from 'react';
@@ -248,7 +116,15 @@ const Genres = () => {
                   >
                     Edit
                   </Button>
-                  <Modal
+                  <EditGenre
+                    isOpen={editGenreModal.isOpen}
+                    onClose={editGenreModal.onClose}
+                    onSubmit={(e) => handleFormSubmit(e, genreItem)}
+                    updateGenre={updateGenre}
+                    handleUpdateGenre={handleUpdateGenre}
+                    isloading={isloading}
+                  />
+                  {/* <Modal
                     initialFocusRef={initialRef}
                     finalFocusRef={finalRef}
                     isOpen={editGenreModal.isOpen}
@@ -285,7 +161,7 @@ const Genres = () => {
                         </form>
                       </ModalBody>
                     </ModalContent>
-                  </Modal>
+                  </Modal> */}
                 </Td>
                 <Td>
                   <Button
